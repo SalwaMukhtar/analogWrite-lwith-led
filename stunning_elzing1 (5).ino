bda5beff47a1f046db19d0d215d0397599651e7b
Salwa Mukhtar <102853846+SalwaMukhtar@users.noreply.github.com>
2022-05-21 12:27:17 +0500

    Add files via upload

---
+++ stunning_elzing1 (5).ino
@@ -0,0 +1,25 @@
+#define led 2
+
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  analogWrite(led,0);
+  delay(500);
+  analogWrite(led,50);
+  delay(500);
+  analogWrite(led,100);
+  delay(500);
+  analogWrite(led,150);
+  delay(500);
+  analogWrite(led,200);
+  delay(500);
+  analogWrite(led,255);
+  delay(500);
+}
